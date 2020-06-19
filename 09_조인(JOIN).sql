72c49fa132d54c043812d1d06cafd8ad716c20b1
caramelsyrup <seojunlim87@gmail.com>
2020-06-19 16:32:52 +0900

    20/06/19

--- 09_조인(JOIN).sql
+++ 09_조인(JOIN).sql
@@ -24,7 +24,7 @@ join departments d --em + dep 테이블 합침
 where d.department_id = 50; --50번 부서만  join해서 보겠당!!
 
 -- 3개 테이블 조인 (2개의 조인 사용)
-select e.employee_id, d.department_name, lo.city
+select e.employee_id, d.department_name, lo.city,
 from employees e 
 join departments d on e.department_id = d.department_id
 join locations lo     on d.location_id = lo.location_id;
@@ -47,67 +47,7 @@ join employees m
 select employee_id, last_name, manager_id 
 from employees where last_name = 'Kumar';
 
--- 외부조인( OUTER JOIN) 
--- > 조인 조건에 일치하지 않는 데이터까지 모두 출력 
--- > LEFT, RIGHT 여부는 LEFT가 먼저 기술한 테이블, RIGHT가 나중에 기술한 테이블
--- > <테이블1> : 왼쪽 테이블, <테이블2> 오른쪽테이블 
--- select에서 앞쪽에 입력되는 값에 해당되는 테이블을 from 테이블로,
--- 뒤에 입력되는 값을 __ outer join 테이블로 해서 적긔 
-
---LEFT OUTER JOIN : 왼쪽행은 일치하지 않아도 다 출력 
-select e.last_name 직원, e.department_id 부서번호, d.department_name 부서명 
-from employees e 
-LEFT OUTER JOIN departments d on e.department_id = d.department_id;
-
-
---RIGHT OUTER JOIN : 오른쪽이 NULL무진쟝 출력
-select e.last_name 직원, e.department_id 부서번호, d.department_name 부서명 
-from employees e 
-RIGHT OUTER JOIN departments d on e.department_id = d.department_id;
-
--- FULL OUTER JOIN : 출력되는 행이 제일 多
-select e.last_name 직원, e.department_id 부서번호, d.department_name 부서명 
-from employees e 
-FULL OUTER JOIN departments d on e.department_id = d.department_id;
-
--- 예제) 앞의 어떤 테이블을 넣을지 순서가 중요한거같네용,,
-select c.country_name, c.country_id, lo.location_id, lo.city 
-from   countries c
-LEFT OUTER join  locations lo
-on c.country_id = lo.country_id
-order by 3 desc;
-
-
-
--- 교차조인 (cross join) : 한 쪽 테이블 모든 행 + 다른 테이블 모든 행 연결
--- cross join에는 on이 없다. 
-
--- 예제)
-select c.country_name 국가 , r.region_name 지역이름 from countries c
-cross join regions r;
-
-
 -- 리뷰 예제
--- 1 ( || 사용 알아두긔 )
-select  d.department_name 부서명 , d.manager_id 매니저번호,
-e.last_name || ' ' || e.first_name "매니저이름(직원이름)", e.phone_number 
-from departments d 
-join employees e on d.manager_id = e.employee_id;
-
--- 2
-select e.employee_id 사원번호, e.hire_date 고용일자, j.job_id 직종 , j.job_title 직책 
-from employees e 
-join jobs j 
-on e.job_id = j.job_id;
-
---★3
-select to_char(e.hire_date, 'YYYY') 입사년도, round(avg(e.salary)) 평균급여  from employees e
-join jobs j
-on e.job_id = j.job_id
-where  j.job_title='Sales Manager'
-group by  to_char(e.hire_date, 'YYYY') order by 1;
--- 그룹을 나누는데, 입사년도를 기준으로 
--- "입사년도그룹별"로 라고 했으니까 ,, 
 
 
 select d.department_name 부서명, lo.city 도시명, c.country_name 국가명
--- 09_조인.sql
+++ 09_조인.sql
@@ -58,4 +58,61 @@ FROM employees e JOIN employees m ON e.manager_id = m.employee_id
 -- 그래서 해당 manager_id를 employee_id에서 찾으면 그 사람이 상사인 것.
 WHERE e.last_name='Smith';
 
+-- 외부조인( OUTER JOIN) 
+-- > 조인 조건에 일치하지 않는 데이터까지 모두 출력.
+-- > LEFT, RIGHT 여부는 LEFT가 먼저 기술한 테이블, RIGHT가 나중에 기술한 테이블
+-- > <테이블1> : 왼쪽 테이블, <테이블2> 오른쪽테이블 
+-- select에서 앞쪽에 입력되는 값에 해당되는 테이블을 from 테이블로,
+-- 뒤에 입력되는 값을 __ outer join 테이블 로 한다.
 
+--LEFT OUTER JOIN : 왼쪽행은 일치하지 않아도 다 출력 
+select e.last_name 직원, e.department_id 부서번호, d.department_name 부서명
+from employees e                                                           -- 왼쪽 테이블.
+LEFT OUTER JOIN departments d on e.department_id = d.department_id;       
+-- Grant가 출력된다.department_id , department_name 값이 없는데 조회,출력이 됨.
+
+--RIGHT OUTER JOIN : 오른쪽이 NULL 출력
+select e.last_name 직원, e.department_id 부서번호, d.department_name 부서명 
+from employees e                                                           
+RIGHT OUTER JOIN departments d on e.department_id = d.department_id;      -- 오른쪽 테이블.
+-- departments테이블에는 department_id가 존재하지만, employees테이블에는 없는 것들이다. 
+-- RIGHT OUTER JOIN이기에, departments가 오른쪽 테이블이고, 이 테이블의 값들을 출력한다.
+-- 결과 값은 당연히 last_name은 NULL이다. 해당 department_id를 가진 직원이 없기 때문이다.
+
+-- FULL OUTER JOIN : 양쪽의 테이블들 모두 참조한다.
+select e.last_name 직원, e.department_id 부서번호, d.department_name 부서명 
+from employees e                                                        -- 왼쪽 테이블.
+FULL OUTER JOIN departments d on e.department_id = d.department_id;   -- 오른쪽 테이블.
+-- JOIN조건 외의 열값들도 테이블이 서로 관계가 된다면, 모두 조회가 된다.
+
+-- 예제.
+SELECT c.country_name AS "국가", c.country_id AS "국가번호",   
+        l.location_id AS "지역번호", l.city AS "도시"
+FROM countries C LEFT OUTER JOIN locations L ON c.country_id = l.country_id ORDER BY 3 DESC ;
+
+-- 교차조인 (cross join) : 한 쪽 테이블 모든 행 * 다른 테이블 모든 행 연결
+-- A테이블의 행이 2개, B테이블의 행이 3개 라면, A-B테이블의 관계에서 2*3=6으로 CROSS JOIN 되면 6개의 행을 가지는 테이블 탄생.
+-- cross join에는 on이 없다. 조건이 필요가 없다. 각테이블의 모든 행을 다 참조하기 때문이다.
+-- 예제.
+SELECT * FROM countries C CROSS JOIN locations L; -- 행의 개수가 575개이다. countries가 25개, locations 23개이기에.
+SELECT c.country_name AS "국가이름", r.region_name AS "지역이름" FROM countries C CROSS JOIN regions R; 
+
+-- 리뷰예제.
+SELECT d.department_name AS "부서명", d.manager_id AS "관리자번호",
+    e.last_name||' '||e.first_name AS " FULL NAME",e.phone_number AS "전화번호"
+FROM departments D JOIN employees E ON d.department_id = e.department_id;
+
+SELECT e.employee_id AS "직원번호" , e.hire_date AS "고용일자",
+        j.job_id AS "직종", j.job_title AS "직책"
+FROM employees E JOIN jobs J ON e.job_id = j.job_id;
+
+SELECT TO_CHAR(e.hire_date,'yyyy') AS "입사년도",avg(e.salary) AS "평균급여"
+FROM employees E JOIN jobs J ON e.job_id = j.job_id 
+WHERE j.job_title = 'Sales Manager' 
+GROUP BY TO_CHAR(e.hire_date,'yyyy')
+ORDER BY 1;
+
+SELECT l.city, ROUND(AVG(E.SALARAY), COUNT(*) 
+FROM departments D JOIN locations L ON d.location_id = l.location_id
+JOIN employees E ON d.department_id = e.employee_id
+GROUP BY l.city ORDER BY 2;
