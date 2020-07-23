d779967e273a959bf81ecfd45a621cb2a2cbd14b
caramelsyrup <seojunlim87@gmail.com>
2020-07-23 16:35:18 +0900

    20/07/23

--- JSP2.sql
+++ JSP2.sql
@@ -6,8 +6,33 @@ CREATE TABLE member(
  userTel NUMBER,
  admin number
 );
-drop table member;
 
-DELETE FROM member;
-
+COMMIT;
 ALTER TABLE member MODIFY userTel VARCHAR2(70);
+
+select * from member;
+
+SELECT COUNT(userid) AS COUNT FROM member;
+
+select * from board;
+select * from board order by num desc;
+delete from board;
+commit;
+SELECT * FROM board WHERE WRITER LIKE '%³ª%';
+CREATE SEQUENCE BOARD_SEQ;
+
+CREATE TABLE commentboard (
+cnum number CONSTRAINT commentboard_cnum_pk PRIMARY KEY,
+userid VARCHAR2(20),
+regdate VARCHAR2(20) DEFAULT sysdate,
+msg VARCHAR2(1000),
+bnum NUMBER
+);
+
+ALTER TABLE commentboard ADD CONSTRAINT commentboard_bnum_fk
+   FOREIGN KEY (bnum) REFERENCES board (num);
+
+CREATE SEQUENCE commentboard_seq;
+delete from commentboard;
+commit;
+select * from commentboard;
