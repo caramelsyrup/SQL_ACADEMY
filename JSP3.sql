986894a79337bdffc904fbefb1e7ac11aae5b621
caramelsyrup <seojunlim87@gmail.com>
2020-07-27 16:32:28 +0900

    20/07/27

---
+++ JSP3.sql
@@ -0,0 +1,9 @@
+insert into jstlmember values('11','pw11','name11');
+insert into jstlmember values('12','pw12','name12');
+insert into jstlmember values('13','pw13','name13');
+insert into jstlmember values('14','pw14','name14');
+insert into jstlmember values('15','pw15','name15');
+commit;
+select * from jstlmember;
+
+select * from address;
